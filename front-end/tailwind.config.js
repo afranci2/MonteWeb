5fbc5c93b8bd6625cb3010cd410ee5b19d4a25f4
El Samp <anthony.francisco.aoct@gmail.com>
2023-02-09 13:44:50 -0500

    fixed tailwind

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './app/**/*.{js,jsx}',
+    './app/**/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './sections/**/*.{js,jsx}',
   ],
   mode: 'jit',
   theme: {
