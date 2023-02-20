1b5c0b31461905e5555ceac2227af5db13a7bde7
El Samp <anthony.francisco.aoct@gmail.com>
2023-02-20 11:43:32 -0500

    Wef

--- front-end/components/tab/liderestab/LideresTab.tsx
+++ front-end/components/tab/liderestab/LideresTab.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Tab from "../Tab";
-import Image from "next/image";
 
 const LideresTab = (props) => {
   return (
-    <div className="bg-red-400 p-8">
-      <Tab className="flex flex-col">      
-        <img src={props.image} alt="" />
+    <div className="w-5/6 m-auto">
+      <Tab className="flex flex-col ">      
+        <img src={props.image} alt="dfsf" />
       </Tab>
       <div className="flex flex-col">
           <p className="font-bold text-2xl">{props.heading}</p>
--- front-end/sections/lideres/Lideres.tsx
+++ front-end/sections/lideres/Lideres.tsx
@@ -1,15 +1,44 @@
 import React from "react";
 import LideresTab from "../../components/tab/liderestab/LideresTab";
-const Pastor = "https://monte-assets.s3.amazonaws.com/img/pastor3.jpg"
+const Pastor = "https://monte-assets.s3.amazonaws.com/img/pastor3.jpg";
+import { MainButton } from "../../components";
+import Header from "../header/Header";
 
 const Lideres = () => {
   return (
     <div>
-      <div className="top"></div>
-      <div className="leader">
-        <LideresTab image={Pastor} heading="Rev. Samuel Francisco" subheading="Presidente" />
+      <div className="w-10/12 p-12 m-auto">
+        <p className="font-serif text-4xl">Nuestros Lideres</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
       </div>
-      <div className="button"></div>
+      <div className="md:flex gap-4 w-11/12 m-auto">
+        <LideresTab
+          image={Pastor}
+          heading="Rev. Samuel Francisco"
+          subheading="Presidente"
+        />
+        <LideresTab
+          image={Pastor}
+          heading="Rev. Samuel Francisco"
+          subheading="Presidente"
+        />
+        <LideresTab
+          image={Pastor}
+          heading="Rev. Samuel Francisco"
+          subheading="Presidente"
+        />
+        <LideresTab
+          image={Pastor}
+          heading="Rev. Samuel Francisco"
+          subheading="Presidente"
+        />
+      </div>
+      <div className="text-white flex justify-center p-12">
+        <MainButton text="Ver Mas" />
+      </div>
     </div>
   );
 };
