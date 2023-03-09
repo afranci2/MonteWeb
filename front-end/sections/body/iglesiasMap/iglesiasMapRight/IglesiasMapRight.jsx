1a6103de19154efcb26c5e8965b223bcf2279b03
El Samp <anthony.francisco.aoct@gmail.com>
2023-03-09 15:14:36 -0500

    fix fetch errors

--- front-end/sections/body/iglesiasMap/iglesiasMapRight/IglesiasMapRight.jsx
+++ front-end/sections/body/iglesiasMap/iglesiasMapRight/IglesiasMapRight.jsx
@@ -1,28 +1,55 @@
-"use client"
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 const IglesiasMapRight = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  const [churches, setChurches] = useState([]);
+
+  async function fetchChurches() {
+    try {
+      const res = await fetch("http://localhost:8000/iglesias", {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+      const data = await res.json();
+      setChurches(data);
+      console.log(data);
+      return data; // parses
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyAOZBGKxwzgj7Jao4tg-qtgM-MgMUOwhOU",
   });
 
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
+  useMemo(() => {
+    fetchChurches();
+  }, []);
+
+  const center = useMemo(() => ({
+    lat: 41.86482006659902,
+    lng: -71.41359964022818,
+  }));
+
+  if (loadError) return <div>wefew</div>;
+  if (!isLoaded)
+    return (
+      <div className="h-screen w-full">
+          <GoogleMap
+            zoom={14}
+            center={center}
+            mapContainerClassName="h-screen w-full"
+          >
+            {churches.map((church) => (
+              <Marker key={church.id} position={church.position} />
+            ))}
+          </GoogleMap>
+      </div>
+    );
 };
-
-function Map() {
-  const coordinates = { lat: 0, lng: 0 };
-
-  return (
-    <div className="h-screen w-full">
-      <GoogleMap
-        zoom={10}
-        center={{ lat: 44, lng: -80 }}
-        mapContainerClassName="h-screen w-full"
-      ></GoogleMap>
-    </div>
-  );
-}
 
 export default IglesiasMapRight;
