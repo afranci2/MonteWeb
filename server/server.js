57b4c3f423f150db7d5075ca46bfebe7c4163733
El Samp <anthony.francisco.aoct@gmail.com>
2023-03-10 10:31:50 -0500

    Wrf

--- server/server.js
+++ server/server.js
@@ -13,46 +13,224 @@ app.use(express.urlencoded({ extended: true })); //
 
 let churches = [
     {
-        "id": 1,
-        "name": "Iglesia Cristiana",
-        "location": "Pawtucket",
-        "description": "A church that focuses on spreading the word of God.",
-        "leadPastor": "Samuel Francisco",
-        "associatePastors": ["Javier Torres"],
-        "services": [
-            { "name": "Servicio Evangelistico", "day": "Sunday", "time": "3:00PM" },
-            { "name": "Estudio Biblico", "day": "Tuesday", "time": "7:00PM" },
-            { "name": "Servicio Especial", "day": "Thursday", "time": "7:30PM" }
+        id: 1,
+        name: "Sede Central",
+        location: "Pawtucket",
+        description: "A church that focuses on spreading the word of God.",
+        leadPastor: "Samuel Francisco",
+        associatePastors: ["Javier Torres"],
+        services: [
+            { name: "Servicio Evangelistico", day: "Sunday", time: "3:00PM" },
+            { name: "Estudio Biblico", day: "Tuesday", time: "7:00PM" },
+            { name: "Servicio Especial", day: "Thursday", time: "7:30PM" },
         ],
-        "socialMedia": {
-            "Facebook": "https://www.facebook.com/iglesia.cristiana",
-            "Instagram": "https://www.instagram.com/iglesia.cristiana",
-            "YouTube": "https://www.youtube.com/channel/UCBVz1_Cristiana"
+        socialMedia: {
+            Facebook: "https://www.facebook.com/iglesia.cristiana",
+            Instagram: "https://www.instagram.com/iglesia.cristiana",
+            YouTube: "https://www.youtube.com/channel/UCBVz1_Cristiana",
         },
-        "address": "1234 Main St",
-        "phone": "401-555-1234",
-        "email": "info@iglesia.com"
+        address: "1234 Main St",
+        phone: "401-555-1234",
+        email: "info@iglesia.com",
+        coordinates: {
+            lat: 41.86482006659902,
+            lng: -71.41359964022818,
+        },
+        images:{
+            main: "https://monte-assets.s3.amazonaws.com/new-image/MS+WEBPAGE/MS+Building+Session/DSC_1662.jpg",
+            supporting:["","",""]
+        }
     },
     {
-        "id": 2,
-        "name": "Iglesia Cristiana 2",
-        "location": "Pawtucket",
-        "description": "A church that focuses on spreading the word of God.",
-        "leadPastor": "Isabel Francisco",
-        "associatePastors": ["Javier Torres"],
-        "services": [
-            { "name": "Servicio Evangelistico", "day": "Sunday", "time": "3:00PM" },
-            { "name": "Estudio Biblico", "day": "Tuesday", "time": "7:00PM" },
-            { "name": "Servicio Especial", "day": "Thursday", "time": "7:30PM" }
+        id: 2,
+        name: "Iglesia Cristiana 2",
+        location: "Pawtucket",
+        description: "A church that focuses on spreading the word of God.",
+        leadPastor: "Isabel Francisco",
+        associatePastors: ["Javier Torres"],
+        services: [
+            { name: "Servicio Evangelistico", day: "Sunday", time: "3:00PM" },
+            { name: "Estudio Biblico", day: "Tuesday", time: "7:00PM" },
+            { name: "Servicio Especial", day: "Thursday", time: "7:30PM" },
         ],
-        "socialMedia": {
-            "Facebook": "https://www.facebook.com/iglesia2.cristiana",
-            "Instagram": "https://www.instagram.com/"
+        socialMedia: {
+            Facebook: "https://www.facebook.com/iglesia2.cristiana",
+            Instagram: "https://www.instagram.com/",
         },
-        "address": "1234 Main St",
-        "phone": "401-555-1234",
-        "email": "info@iglesia.com"
-    }]
+        address: "1234 Main St",
+        phone: "401-555-1234",
+        email: "info@iglesia.com",
+        coordinates: {
+            lat: 41.84142288169135, lng: -71.47616923108586
+        },
+        images:{
+            main: "https://monte-assets.s3.amazonaws.com/new-image/MS+WEBPAGE/MS+Building+Session/DSC_5188.jpg",
+            supporting:["","",""]
+        }
+    },
+    {
+        id: 3,
+        name: "New Bedford",
+        location: "New Bedford",
+        description: "A church that focuses on spreading the word of God.",
+        leadPastor: "Isabel Francisco",
+        associatePastors: ["Javier Torres"],
+        services: [
+            { name: "Servicio Evangelistico", day: "Sunday", time: "3:00PM" },
+            { name: "Estudio Biblico", day: "Tuesday", time: "7:00PM" },
+            { name: "Servicio Especial", day: "Thursday", time: "7:30PM" },
+        ],
+        socialMedia: {
+            Facebook: "https://www.facebook.com/iglesia2.cristiana",
+            Instagram: "https://www.instagram.com/",
+        },
+        address: "1234 Main St",
+        phone: "401-555-1234",
+        email: "info@iglesia.com",
+        coordinates: {
+            lat: 41.65690967390518, lng: -70.92700191534158
+        },
+        images:{
+            main: "",
+            supporting:["","",""]
+        }
+    },
+    {
+        id: 4,
+        name: "New Bedford",
+        location: "Pawtucket",
+        description: "A church that focuses on spreading the word of God.",
+        leadPastor: "Isabel Francisco",
+        associatePastors: ["Javier Torres"],
+        services: [
+            { name: "Servicio Evangelistico", day: "Sunday", time: "3:00PM" },
+            { name: "Estudio Biblico", day: "Tuesday", time: "7:00PM" },
+            { name: "Servicio Especial", day: "Thursday", time: "7:30PM" },
+        ],
+        socialMedia: {
+            Facebook: "https://www.facebook.com/iglesia2.cristiana",
+            Instagram: "https://www.instagram.com/",
+        },
+        address: "1234 Main St",
+        phone: "401-555-1234",
+        email: "info@iglesia.com",
+        coordinates: {
+            lat: 41.65690967390518, lng: -70.92700191534158
+        },
+        images:{
+            main: "",
+            supporting:["","",""]
+        }
+    },
+    {
+        id: 5,
+        name: "New Bedford",
+        location: "Pawtucket",
+        description: "A church that focuses on spreading the word of God.",
+        leadPastor: "Isabel Francisco",
+        associatePastors: ["Javier Torres"],
+        services: [
+            { name: "Servicio Evangelistico", day: "Sunday", time: "3:00PM" },
+            { name: "Estudio Biblico", day: "Tuesday", time: "7:00PM" },
+            { name: "Servicio Especial", day: "Thursday", time: "7:30PM" },
+        ],
+        socialMedia: {
+            Facebook: "https://www.facebook.com/iglesia2.cristiana",
+            Instagram: "https://www.instagram.com/",
+        },
+        address: "1234 Main St",
+        phone: "401-555-1234",
+        email: "info@iglesia.com",
+        coordinates: {
+            lat: 41.65690967390518, lng: -70.92700191534158
+        },
+        images:{
+            main: "",
+            supporting:["","",""]
+        }
+    },
+    {
+        id: 6,
+        name: "New Bedford",
+        location: "Pawtucket",
+        description: "A church that focuses on spreading the word of God.",
+        leadPastor: "Isabel Francisco",
+        associatePastors: ["Javier Torres"],
+        services: [
+            { name: "Servicio Evangelistico", day: "Sunday", time: "3:00PM" },
+            { name: "Estudio Biblico", day: "Tuesday", time: "7:00PM" },
+            { name: "Servicio Especial", day: "Thursday", time: "7:30PM" },
+        ],
+        socialMedia: {
+            Facebook: "https://www.facebook.com/iglesia2.cristiana",
+            Instagram: "https://www.instagram.com/",
+        },
+        address: "1234 Main St",
+        phone: "401-555-1234",
+        email: "info@iglesia.com",
+        coordinates: {
+            lat: 41.65690967390518, lng: -70.92700191534158
+        },
+        images:{
+            main: "",
+            supporting:["","",""]
+        }
+    },
+    {
+        id: 7,
+        name: "New Bedford",
+        location: "Pawtucket",
+        description: "A church that focuses on spreading the word of God.",
+        leadPastor: "Isabel Francisco",
+        associatePastors: ["Javier Torres"],
+        services: [
+            { name: "Servicio Evangelistico", day: "Sunday", time: "3:00PM" },
+            { name: "Estudio Biblico", day: "Tuesday", time: "7:00PM" },
+            { name: "Servicio Especial", day: "Thursday", time: "7:30PM" },
+        ],
+        socialMedia: {
+            Facebook: "https://www.facebook.com/iglesia2.cristiana",
+            Instagram: "https://www.instagram.com/",
+        },
+        address: "1234 Main St",
+        phone: "401-555-1234",
+        email: "info@iglesia.com",
+        coordinates: {
+            lat: 41.65690967390518, lng: -70.92700191534158
+        },
+        images:{
+            main: "",
+            supporting:["","",""]
+        }
+    },
+    {
+        id: 8,
+        name: "Newfwefew",
+        location: "Pawtucket",
+        description: "A church that focuses on spreading the word of God.",
+        leadPastor: "Isabel Francisco",
+        associatePastors: ["Javier Torres"],
+        services: [
+            { name: "Servicio Evangelistico", day: "Sunday", time: "3:00PM" },
+            { name: "Estudio Biblico", day: "Tuesday", time: "7:00PM" },
+            { name: "Servicio Especial", day: "Thursday", time: "7:30PM" },
+        ],
+        socialMedia: {
+            Facebook: "https://www.facebook.com/iglesia2.cristiana",
+            Instagram: "https://www.instagram.com/",
+        },
+        address: "1234 Main St",
+        phone: "401-555-1234",
+        email: "info@iglesia.com",
+        coordinates: {
+            lat: 41.65690967390518, lng: -70.92700191534158
+        },
+        images:{
+            main: "",
+            supporting:["","",""]
+        }
+    },
+];
 
 
 app.get("/", (req, res) => {
