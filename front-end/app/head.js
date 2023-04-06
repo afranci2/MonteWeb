d55b1edbadf456cfa5eef4ff5052b4f5606b33a4
El Samp <anthony.francisco.aoct@gmail.com>
2023-04-06 15:31:54 -0400

    wef

--- front-end/app/head.js
+++ front-end/app/head.js
@@ -2,7 +2,7 @@ const Head = () => (
   <>
     <title>Movimiento Pentecostal Monte Sinai</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="desciption" content="Enciende el Fuego en ti. Join us today!"/>
+    <meta name="description" content="Enciende el Fuego en ti. Join us today!"/>
     <link rel="icon" href="" />
   </>
 );
