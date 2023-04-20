8628276e34864657e6e7203d6903a8ace0f4fe24
El Samp <anthony.francisco.aoct@gmail.com>
2023-04-20 18:03:56 -0400

    fix leader conocernos

--- front-end/app/conocernos/page.jsx
+++ front-end/app/conocernos/page.jsx
@@ -263,7 +263,7 @@ const page = async () => {
                 <Link href="/conocernos/historia/experiencias-de-westminster">
                   <div className=" p-4">
                     <p className="font-bold text-xs">
-                      La Experiencias de Westminster St.
+                      Las Experiencias de Westminster St.
                     </p>
                   </div>
                 </Link>
