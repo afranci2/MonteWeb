a0b6ee9700ef4cd91784efba79bf1b1a8622a6ab
El Samp <anthony.francisco.aoct@gmail.com>
2023-04-20 16:06:35 -0400

    admin

--- front-end/components/admindashboard/Events.tsx
+++ front-end/components/admindashboard/Events.tsx
@@ -1,7 +1,10 @@
-"use client"
-import React, {useState, useEffect} from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
 
-async function fetchCredentials(e) {
+const Events = () => {
+  const [data, setData] = useState();
+
+  async function fetchEvents() {
     try {
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_SERVER_URL}/get-all-events`,
@@ -15,18 +18,59 @@ async function fetchCredentials(e) {
       );
 
       const data = await res.json();
+      setData(data);
+
       return data;
     } catch (error) {
       console.log(error);
     }
   }
 
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
-  
-const Events = () => {
+  console.log(data);
+
+  function clickHandler() {
+    console.log("hey");
+  }
+
   return (
-    <div>Events</div>
-  )
-}
+    <div>
+      <p>Events</p>
 
-export default Events
+      <table className="table-auto border-2">
+        <tr className="text-left">
+          <th>Edit</th>
+          <th>Name</th>
+          <th>Location</th>
+          <th>Description</th>
+          <th>Image</th>
+          <th>Link</th>
+        </tr>
+        {data != undefined
+          ? data.map((event) => {
+              return (
+                <tr className="" key={event.id}>
+                  <td
+                    onClick={clickHandler}
+                    className="cursor-pointer hover:bg-gray-200"
+                  >
+                    Yes/No
+                  </td>
+                  <td>{event.name}</td>
+                  <td>{event.location}</td>
+                  <td>{event.description}</td>
+                  <td>{event.image}</td>
+                  <td>{event.link}</td>
+                </tr>
+              );
+            })
+          : null}
+      </table>
+    </div>
+  );
+};
+
+export default Events;
