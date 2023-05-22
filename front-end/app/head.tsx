6566f597f86a7211d50d7fb4b8567cecf78ad390
El Samp <anthony.francisco.aoct@gmail.com>
2023-05-22 18:18:45 -0400

    favicon

--- front-end/app/head.tsx
+++ front-end/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Movimiento Pentecostal Monte Sinai</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Enciende el Fuego en ti. Join us today!"/>
-      <link rel="icon" href="" />
+      <link rel="icon" href="/new-logo.png" />
     </>
   )
 }
