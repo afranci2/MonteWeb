46d563bad8e1ef0838915843e8a1741b9a802222
El Samp <anthony.francisco.aoct@gmail.com>
2023-05-23 08:25:18 -0400

    new logo

--- front-end/app/layout.jsx
+++ front-end/app/layout.jsx
@@ -7,7 +7,7 @@ const RootLayout = ({ children }) => (
       <title>Movimiento Pentecostal Monte Sinai</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Enciende el Fuego en ti. Join us today!"/>
-      <link rel="icon" href="mobile_logo.webp" />
+      <link rel="icon" href="new-logo.png" />
       <link
         rel="stylesheet"
         href="https://stijndv.com/fonts/Eudoxus-Sans.css"
